0ce132c9a681b9551d21564e5611d26e87107844
Sammy <andrews9@mail.uc.edu>
2026-02-17 08:55:30 -0500

    added vars

--- src/Lab_05_Java_Conditionals.java
+++ src/Lab_05_Java_Conditionals.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
 public class Lab_05_Java_Conditionals {
     //declare vars
+    Scanner in =new Scanner(System.in); //'in' is our scanner var
+    double wage = 0;
+
+
+
 
 }
